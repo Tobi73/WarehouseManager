dbc4676d656253f3b9147d21ab04e9eec25d3585
Andrey <gman007@yandex.ru>
2017-12-18 01:19:31 +0400

    Implemented export download on front end

--- application/src/components/Header.vue
+++ application/src/components/Header.vue
@@ -14,7 +14,7 @@
       </v-flex>
 
       <v-flex xs12 offset-md1 md1>
-        <v-btn block color="light-blue lighten-1" @click.native="showModal = true">Export</v-btn>
+        <v-btn block color="light-blue lighten-1" @click.native="exportProducts()">Export</v-btn>
       </v-flex>
 
       <v-flex xs12 offset-md1 md1>
@@ -25,7 +25,9 @@
 </template>
 
 <script>
-  import Authentication from '@/components/pages/Authentication'
+  import Authentication from '@/components/pages/Authentication';
+  import Axios from 'axios';
+  const WarehouseManagerAPI = `http://${window.location.hostname}:3002`;
   export default {
     data () {
       return {
@@ -40,6 +42,25 @@
       },
       addProduct () {
         this.$router.push('/product')
+      },
+      exportProducts () {
+        Axios.get(`${WarehouseManagerAPI}/api/v1/products/export`,  {
+            headers: { 'Authorization': Authentication.getAuthenticationHeader(this) },
+            params: { user_id: this.$cookie.get('user_id') },
+            responseType: 'arraybuffer'
+        })
+        .then(response => {
+            let blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' } )
+            let link = document.createElement('a')
+            link.href = window.URL.createObjectURL(blob)
+            link.download = 'export.xlsx'
+            link.click()
+            window.URL.revokeObjectURL(blob);
+            link.remove();
+        }).catch(({ response: {data} }) => {
+            this.snackbar = true;
+            this.message = data.message;
+        });
       }
     }
   }
